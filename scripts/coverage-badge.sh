7bc9c35253d2226dd8b1d54e5b9341fc970ac782
Jakob Hohlfeld <jakob.hohlfeld@netronaut.de>
2024-04-26 21:52:37 -0300

    cicd: fix publish workflow

--- scripts/coverage-badge.sh
+++ scripts/coverage-badge.sh
@@ -2,4 +2,5 @@
 
 coverage=$(bun run test:coverage 2>&1 | grep "All files" | cut -d "|" -f3 | xargs)
 color=$([[ $coverage -gt 80 ]] && echo "green" || echo "red")
-wget -O coverage.svg "https://img.shields.io/badge/Coverage-$coverage%-$color"
+mkdir docs
+wget -O docs/coverage.svg "https://img.shields.io/badge/Coverage-$coverage%-$color"
