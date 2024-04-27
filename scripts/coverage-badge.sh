31d3d56e59e83ae00b6af1116519fbdf27267eb9
Jakob Hohlfeld <jakob.hohlfeld@netronaut.de>
2024-04-26 23:07:53 -0300

    update workflow

--- scripts/coverage-badge.sh
+++ scripts/coverage-badge.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 coverage=$(bun run test:coverage 2>&1 | grep "All files" | cut -d "|" -f3 | xargs)
-color=$([[ $coverage -gt 80 ]] && echo "green" || echo "red")
-mkdir docs
-wget -O docs/coverage.svg "https://img.shields.io/badge/Coverage-$coverage%-$color"
+color=$([[ 1 -eq "$(echo "$coverage >= 80" | bc)" ]] && echo "green" || echo "red")
+mkdir -p coverage
+wget -O coverage/coverage.svg "https://img.shields.io/badge/Coverage-$coverage%-$color"
