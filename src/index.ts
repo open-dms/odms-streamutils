647595d7173f4b2685e13b1517177fc90e10b975
Jakob Hohlfeld <jakob.hohlfeld@netronaut.de>
2024-04-30 13:34:06 -0300

    refactor: rename split to text

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from "./http";
 export * from "./json";
 export * from "./props";
-export * from "./split";
+export * from "./text";
 export * from "./throttle";
 export * from "./toString";
