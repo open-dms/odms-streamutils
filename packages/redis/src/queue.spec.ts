433c28251c35dd1ce22b2ef2ae5422bd1399d038
Jakob Hohlfeld <jakob.hohlfeld@netronaut.de>
2024-05-27 22:45:22 -0300

    feat: add read queue

--- packages/redis/src/queue.spec.ts
+++ packages/redis/src/queue.spec.ts
@@ -1,26 +1,112 @@
+import { nextTick } from "@repo/test";
 import { afterEach, describe, expect, it, mock } from "bun:test";
 import { finished } from "node:stream/promises";
-import { redisWriteQueue } from ".";
+import { redisReadQueue, redisWriteQueue } from ".";
 
 const mockClient = {
-  connect: () => Promise.resolve(),
+  connect: mock(() => Promise.resolve()),
   on: () => {},
-  rPush: mock((key: string, message: string) => Promise.resolve()),
+  quit: () => Promise.resolve(),
+  rPush: mock().mockResolvedValue(undefined),
+  brPop: mock().mockResolvedValue(undefined),
 };
 
 mock.module("redis", () => ({
   createClient: () => mockClient,
 }));
 
-describe("redis queue", () => {
+describe("redis", () => {
   afterEach(() => {
+    mockClient.connect.mockReset();
     mockClient.rPush.mockClear();
   });
 
-  it("should write to redis", async () => {
-    const stream = await redisWriteQueue("redis://localhost", "stream_key");
-    await finished(stream.end("test1"));
-    expect(mockClient.rPush).toHaveBeenCalled();
-    expect(mockClient.rPush.mock.lastCall).toEqual(["stream_key", "test1"]);
+  describe("write queue", () => {
+    it("should write to queue", async () => {
+      const stream = await redisWriteQueue({
+        url: "redis://localhost",
+        channel: "queue_key",
+      });
+      await finished(stream.end("test1"));
+      expect(mockClient.rPush).toHaveBeenCalled();
+      expect(mockClient.rPush.mock.lastCall).toEqual(["queue_key", "test1"]);
+    });
+  });
+
+  describe("read queue", () => {
+    it("should read from queue", async () => {
+      const stream = await redisReadQueue({
+        url: "redis://localhost",
+        channel: "queue_key",
+      });
+      const response: Array<string> = [];
+
+      stream.on("data", (data) => {
+        response.push(data);
+        stream.push(null);
+      });
+
+      mockClient.brPop.mockResolvedValueOnce({
+        key: "queue_key",
+        element: "response1",
+      });
+
+      await finished(stream);
+      expect(response).toEqual(["response1"]);
+    });
+
+    it("should handle connection error", async () => {
+      mockClient.connect.mockRejectedValueOnce(Error("Connection error"));
+      const stream = await redisReadQueue({
+        url: "redis://localhost",
+        channel: "queue_key",
+      });
+      await nextTick();
+      expect(stream.errored?.message).toBe("Failed to connect to redis client");
+      expect(stream.errored).toBeInstanceOf(Error);
+      expect(stream.errored?.cause).toEqual(Error("Connection error"));
+    });
+
+    it("should throw if abort signal is not sent before destroying", async () => {
+      const controller = new AbortController();
+      const stream = await redisReadQueue({
+        url: "redis://localhost",
+        channel: "queue_key",
+        signal: controller.signal,
+      });
+
+      stream.destroy();
+      expect(finished(stream)).rejects.toThrowError();
+    });
+
+    it("should abort", async () => {
+      const controller = new AbortController();
+      const stream = await redisReadQueue({
+        url: "redis://localhost",
+        channel: "queue_key",
+        signal: controller.signal,
+      });
+
+      stream.resume();
+      controller.abort();
+      await finished(stream);
+      expect(stream.closed).toBeTrue();
+    });
+
+    it("should handle read error", async () => {
+      mockClient.brPop.mockImplementationOnce(() =>
+        Promise.reject(Error("Some redis error"))
+      );
+
+      const stream = await redisReadQueue({
+        url: "redis://localhost",
+        channel: "queue_key",
+      });
+
+      expect(finished(stream.resume())).rejects.toHaveProperty(
+        "cause.message",
+        "Some redis error"
+      );
+    });
   });
 });
--- packages/redis/src/queue.ts
+++ packages/redis/src/queue.ts
@@ -1,4 +1,4 @@
-import { Writable } from "node:stream";
+import { Readable, Writable } from "node:stream";
 import { createClient } from "redis";
 
 export const redisWriteQueue = async (url: string, channel: string) => {
@@ -28,7 +28,65 @@ export const redisWriteQueue = async (url: string, channel: string) => {
             : Error("Redis RPUSH error", { cause: err })
         );
       }
-      callback();
+    },
+  });
+};
+
+export const redisReadQueue = async ({
+  url,
+  channel,
+  signal,
+}: {
+  url: string;
+  channel: string;
+  signal?: AbortSignal;
+}) => {
+  let controller: AbortController;
+  const client = createClient({ url });
+
+  return new Readable({
+    objectMode: true,
+    async construct(callback) {
+      client.on("error", (err) => this.emit("error", err));
+      if (!signal) {
+        controller = new AbortController();
+        signal = controller.signal;
+      }
+      signal.onabort = () => this.push(null);
+      try {
+        await client.connect();
+        callback();
+      } catch (err) {
+        callback(Error("Failed to connect to redis client", { cause: err }));
+      }
+    },
+    async destroy(_error, callback) {
+      let error = null;
+      if (!signal?.aborted) {
+        if (controller) {
+          controller.abort();
+        } else {
+          error = Error(
+            "Before destroying the stream, send the abort signal first."
+          );
+        }
+      }
+
+      await client.quit();
+      callback(error);
+    },
+    async read() {
+      try {
+        let paused = false;
+        while (!paused && !signal?.aborted) {
+          const data = await client.brPop(channel, readQueueTimeout / 1000);
+          if (data) {
+            paused = !this.push(data.element);
+          }
+        }
+      } catch (err) {
+        this.emit("error", Error("Redis BRPOP error", { cause: err }));
+      }
     },
   });
 };
